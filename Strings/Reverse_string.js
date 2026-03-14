507c187b3794be69a39804778fb1cd866a77ac2b
ankitsingh-projects <ankitsingh123iec@gmail.com>
2026-03-14 10:08:39 +0530

    Strings coding

--- Strings/Reverse_string.js
+++ Strings/Reverse_string.js
@@ -1,8 +1,24 @@
+//Using for-loop
+
 let str = "hello";
 let reversed = "";
 
 for(let i=str.length-1; i>=0; i--){
     reversed += str[i];
 }
-
 console.log(reversed);
+
+// without using for-loop
+
+let string = "github";
+let reverse = string.split("").reverse().join("");
+console.log(reverse);
+
+//Using function
+function reverseString(str){
+    if(str ===""){
+        return ""
+    }
+return reverseString(str.substr(1)) +str[0];
+}
+console.log(reverseString("QAankitSingh"));
